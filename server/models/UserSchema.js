258b7dca096e64382bc170ae1003a86f2799b451
nalin243 <devpriyanalin@gmail.com>
2022-09-16 17:57:42 +0530

    Added BookSchema and Library in UserSchema

--- server/models/UserSchema.js
+++ server/models/UserSchema.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 const {Schema,model} = mongoose;
 
+const BookSchema = new Schema({
+	name: String,
+	Pages: Number,
+	Read: Boolean,
+	Author: String,
+	Description: String
+})
+
 const UserSchema = new Schema({
 	username: String,
-	password: String
+	password: String,
+	Library: [BookSchema]
 })
 
 const User = model('User',UserSchema)
